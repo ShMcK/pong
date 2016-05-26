b330154fa77513648d81f05cb91a1884b16d2180
ShMcK <shawn.j.mckay@gmail.com>
2016-05-26 13:57:44 -0700

    Step 2.3: draw paddles

--- src/game.js
+++ src/game.js
@@ -13,11 +13,13 @@ export default class Game {
     this.player1.y = (this.height / 2) - (this.player1.height / 2);
     this.player2 = new Paddle(this.width - 14, 0);
     this.player2.y = (this.height / 2) - (this.player2.height / 2);
-    console.log(this.player1, this.player2);
   }
   draw() {
     this.context.clearRect(0, 0, this.width, this.height);
     this.context.fillRect(this.width/2, 0, 2, this.height);
+
+    this.player1.draw(this.context);
+    this.player2.draw(this.context)
   }
   render() {
     if (this.paused) { return; }
