2de004e8baa7223d2cb7ecbb23bba5ab9506b981
ShMcK <shawn.j.mckay@gmail.com>
2016-05-26 13:34:26 -0700

    Step 1.2: add Game draw method

--- src/game.js
+++ src/game.js
@@ -8,4 +8,8 @@ class Game {
     this.context = canvas.getContext('2d');
     this.context.fillStyle = 'white';
   }
+  draw() {
+    this.context.clearRect(0, 0, this.width, this.height);
+    this.context.fillRect(this.width/2, 0, 2, this.height);
+  }
 }
