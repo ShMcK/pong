35340fb23bd4be5c614818839d5ac12a4a4f69a8
ShMcK <shawn.j.mckay@gmail.com>
2016-05-26 13:35:47 -0700

    Step 1.3: add Game render method

--- src/game.js
+++ src/game.js
@@ -7,9 +7,13 @@ class Game {
     this.height = canvas.height;
     this.context = canvas.getContext('2d');
     this.context.fillStyle = 'white';
+    this.paused = false;
   }
   draw() {
     this.context.clearRect(0, 0, this.width, this.height);
     this.context.fillRect(this.width/2, 0, 2, this.height);
+  }
+  render() {
+    if (this.paused) { return; }
   }
 }
