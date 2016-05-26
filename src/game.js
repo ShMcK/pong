52695501e0aa8df234113372a28cee8683436996
ShMcK <shawn.j.mckay@gmail.com>
2016-05-26 13:32:15 -0700

    Step 1.1: setup canvas height, width & context

--- src/game.js
+++ src/game.js
@@ -3,6 +3,9 @@ require('./game.css');
 class Game {
   constructor() {
     const canvas = document.getElementById('game');
-
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.context = canvas.getContext('2d');
+    this.context.fillStyle = 'white';
   }
 }
