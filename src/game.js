c2fc818055cf166e8bbeaa9ef2a5ebe32083041e
ShMcK <shawn.j.mckay@gmail.com>
2016-05-26 13:57:29 -0700

    Step 2.2: create player 1 & player 2 paddles

--- src/game.js
+++ src/game.js
@@ -1,3 +1,5 @@
+import Paddle from './Paddle';
+
 export default class Game {
   constructor() {
     const canvas = document.getElementById('game');
@@ -6,6 +8,12 @@ export default class Game {
     this.context = canvas.getContext('2d');
     this.context.fillStyle = 'white';
     this.paused = false;
+
+    this.player1 = new Paddle(10, 0);
+    this.player1.y = (this.height / 2) - (this.player1.height / 2);
+    this.player2 = new Paddle(this.width - 14, 0);
+    this.player2.y = (this.height / 2) - (this.player2.height / 2);
+    console.log(this.player1, this.player2);
   }
   draw() {
     this.context.clearRect(0, 0, this.width, this.height);
